31c9ca002d13fa008eba308a63a38591d654dae4
Hbbolaji <bolaji1050@gmail.com>
2026-05-30 00:19:04 +0100

    implemented shell loop and exit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use std::io::{ Write, stdin, stdout};
 
 fn main() {
-    print!("$ ");
-    stdout().flush().unwrap();
-    
-    let mut command = String::new();
-    stdin().read_line(&mut command).unwrap();
-    let output = format!("{}: command not found", command.trim_end());
-    println!("{}", output)
+    loop {
+        print!("$ ");
+        stdout().flush().unwrap();
+        
+        let mut command = String::new();
+        stdin().read_line(&mut command).unwrap();
+
+        command = command.trim().to_string();
+
+        if command.starts_with("echo") {
+            println!("{}", &command[5..].trim());
+            continue;
+        }
+
+        if command == "exit" {
+            break;
+        }
+
+        let output = format!("{}: command not found", command);
+        println!("{}", output)
+    }
 }
