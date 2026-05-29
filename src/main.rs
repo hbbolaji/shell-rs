7ad8507a223f6b97e667f1985563c5ac7e8a6a02
Hbbolaji <bolaji1050@gmail.com>
2026-05-29 11:37:22 +0100

    accepting command with stdin read line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{ Write, stdin, stdout};
+
 fn main() {
-    println!("Hello, world!");
+    print!("$ ");
+    stdout().flush().unwrap();
+    
+    let mut command = String::new();
+    stdin().read_line(&mut command).unwrap();
+    let output = format!("{}: command not found", command.trim_end());
+    println!("{}", output)
 }
