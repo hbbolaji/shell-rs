9b1b56684b66a826bc3d89d9c9338ddd441a14e7
Hbbolaji <bolaji1050@gmail.com>
2026-05-30 23:46:04 +0100

    implemented command execution

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use std::{
 
 use is_executable::IsExecutable;
 
+struct Executable {
+    cmd: String,
+    path: String,
+}
+
 fn main() {
     let shell_builtin = ["echo", "type", "exit"];
 
@@ -13,23 +18,29 @@ fn main() {
         print!("$ ");
         stdout().flush().unwrap();
 
-        let mut command = String::new();
-        stdin().read_line(&mut command).unwrap();
+        let mut user_input = String::new();
+        stdin().read_line(&mut user_input).unwrap();
 
-        command = command.trim().to_string();
+        user_input = user_input.trim().to_string();
+        let user_input_list = user_input.split(" ").collect::<Vec<&str>>();
+        let command = user_input_list[0];
+        let args = &user_input_list[1..];
 
-        if command.starts_with("echo") {
-            println!("{}", &command[5..].trim());
+        if command == "echo" {
+            println!("{}", &user_input[5..].trim());
             continue;
         }
 
-        if command.starts_with("type") {
-            let builtin = &command[5..].trim();
-
-            if shell_builtin.contains(builtin) {
-                println!("{} is a shell builtin", builtin);
-            } else {
-                find_executable(&builtin);
+        if command == "type" {
+            for arg in args {
+                if shell_builtin.contains(arg) {
+                    println!("{} is a shell builtin", arg);
+                } else {
+                    match find_executable(arg) {
+                        Some(value) => println!("{}", value.path),
+                        _ => println!("{} not found", arg),
+                    };
+                }
             }
             continue;
         }
@@ -38,12 +49,22 @@ fn main() {
             break;
         }
 
-        let output = format!("{}: command not found", command);
-        println!("{}", output)
+        let executable_cmd = find_executable(command);
+        match executable_cmd {
+            Some(value) => {
+                let output = std::process::Command::new(value.cmd)
+                    .args(args)
+                    .status()
+                    .unwrap();
+
+                println!("{:?}", output.to_string());
+            }
+            None => println!("{}: command not found", command),
+        }
     }
 }
 
-fn find_executable(executable: &str) {
+fn find_executable(executable: &str) -> Option<Executable> {
     let paths = env::var_os("PATH").unwrap();
     let path_list: Vec<&str> = paths.to_str().unwrap().split(":").collect();
     let path_dir: Vec<String> = path_list
@@ -53,9 +74,11 @@ fn find_executable(executable: &str) {
         .collect();
 
     if path_dir.is_empty() {
-        println!("{} not found", executable);
-        return;
+        return None;
     }
 
-    println!("{}", &path_dir[0])
+    Some(Executable {
+        cmd: executable.into(),
+        path: path_dir[0].to_string(),
+    })
 }
