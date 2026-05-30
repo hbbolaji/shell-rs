7f76208f86a9fde146fc68e8fe86f85db8e8bd04
Hbbolaji <bolaji1050@gmail.com>
2026-05-30 01:31:11 +0100

    implemented type command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{ Write, stdin, stdout};
 
 fn main() {
+
+    let shell_builtin = ["echo", "type", "exit"];
     loop {
         print!("$ ");
         stdout().flush().unwrap();
@@ -12,6 +14,17 @@ fn main() {
 
         if command.starts_with("echo") {
             println!("{}", &command[5..].trim());
+            continue;
+        }
+
+        if command.starts_with("type") {
+            let builtin = &command[5..].trim();
+
+            if shell_builtin.contains(builtin) {
+                println!("{} is a shell builtin", builtin);
+            } else {
+                println!("{} not found", builtin)
+            }
             continue;
         }
 
