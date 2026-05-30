4607bdebd974f0f2e9544240db93adb956dcd1a6
Hbbolaji <bolaji1050@gmail.com>
2026-05-30 04:29:00 +0100

    implemented function to locate executable files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::io::{ Write, stdin, stdout};
+use std::{
+    env,
+    io::{Write, stdin, stdout},
+    path::Path,
+};
+
+use is_executable::IsExecutable;
 
 fn main() {
-
     let shell_builtin = ["echo", "type", "exit"];
+
     loop {
         print!("$ ");
         stdout().flush().unwrap();
-        
+
         let mut command = String::new();
         stdin().read_line(&mut command).unwrap();
 
@@ -23,7 +29,7 @@ fn main() {
             if shell_builtin.contains(builtin) {
                 println!("{} is a shell builtin", builtin);
             } else {
-                println!("{} not found", builtin)
+                find_executable(&builtin);
             }
             continue;
         }
@@ -35,4 +41,21 @@ fn main() {
         let output = format!("{}: command not found", command);
         println!("{}", output)
     }
+}
+
+fn find_executable(executable: &str) {
+    let paths = env::var_os("PATH").unwrap();
+    let path_list: Vec<&str> = paths.to_str().unwrap().split(":").collect();
+    let path_dir: Vec<String> = path_list
+        .iter()
+        .map(|path| format!("{}/{}", path, executable))
+        .filter(|item| Path::new(item).is_file() && Path::new(item).is_executable())
+        .collect();
+
+    if path_dir.is_empty() {
+        println!("{} not found", executable);
+        return;
+    }
+
+    println!("{}", &path_dir[0])
 }
